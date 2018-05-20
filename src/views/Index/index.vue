26c31f05973d0531b0dae25307f9e0316c225abf
Vincent <1312065793@qq.com>
2018-05-21 00:47:02 +0800

    首页自适应屏幕

--- src/views/Index/index.vue
+++ src/views/Index/index.vue
@@ -1,87 +1,86 @@
 <template>
-  <div class="system-home">
-    <div class="home-absolute">
-      <div class="home-top clearfix">
-        <a class="meau-icon">
-          <i class="iconfont">&#xe655;</i>
-        </a>
-        <div class="user-info f-r">
-          <img src="@/assets/index/header.png">
-          <div class="text-info">
-            <p>某某某</p>
-            <p>高级管理员</p>
+  <div class="container">
+    <!-- header -->
+    <div class="index-header clearfix">
+      <a class="menu-link">
+        <i class="iconfont">&#xe655;</i>
+      </a>
+      <div class="user-info f-r">
+        <img src="@/assets/index/header.png">
+        <span>Admin</span>
+      </div>
+    </div>
+    <!-- main-wrap -->
+    <div class="index-module">
+      <div class="clearfix">
+        <div class="grid-content gird-1">
+          <div class="gird-wrap">
+            <div class="trans-middle">
+              <p class="p1"><i class="iconfont">&#xe7b9;</i></p>
+              <p>GIS地理信息系统</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-content gird-2">
+          <div class="gird-wrap">
+            <div class="trans-middle">
+              <p class="p1"><i class="iconfont">&#xe613;</i></p>
+              <p>资产管理系统</p>
+            </div>
           </div>
         </div>
       </div>
-      <div class="home-center">
-        <el-row :gutter="20" class="mode-block">
-          <el-col :span="12">
-            <div class="grid-content model-a">
-              <div class="trans-middle">
-                <p class="p1"><i class="iconfont">&#xe7b9;</i></p>
-                <p>GIS地理信息系统</p>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content model-b">
-              <div class="trans-middle">
-                <p class="p1"><i class="iconfont">&#xe691;</i></p>
-                <p>道路照明系统</p>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content model-c">
-              <div class="trans-middle">
-                <p class="p1"><i class="iconfont">&#xe613;</i></p>
-                <p>资产管理系统</p>
-              </div>
-            </div>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20" class="mode-block">
-          <el-col :span="7">
-            <div class="grid-content model-d">
-              <div class="trans-middle">
-                <p class="p1"><i class="iconfont f-mid">&#xe6f5;</i></p>
-                <p>能耗分析</p>
-              </div>
-            </div>
-            <div class="grid-content model-d1">
-              <div class="trans-middle">
-                <p class="p1"><i class="iconfont f-mid">&#xe603;</i></p>
-                <p>用户中心</p>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="5">
-            <div class="grid-content model-e">
-              <div class="trans-middle">
+      <div class="clearfix">
+        <div class="grid-content gird-9 clearfix">
+          <div class="grid-content gird-3">
+            <div class="gird-wrap">
+              <div class="trans-middle f-mid">
                 <p class="p1"><i class="iconfont">&#xe615;</i></p>
                 <p>事件报警</p>
               </div>
             </div>
-          </el-col>
-          <el-col :span="6" >
-            <div class="grid-content model-f">
-              <div class="trans-middle">
+          </div>
+          <div class="grid-content gird-4">
+            <div class="gird-wrap">
+              <div class="trans-middle f-mid">
                 <p class="p1"><i class="iconfont">&#xe664;</i></p>
                 <p>工单管理</p>
               </div>
             </div>
-          </el-col>
-          <el-col :span="6">
-            <router-link to="/ProjectAdmin">
-              <div class="grid-content model-g">
-               <div class="trans-middle">
+          </div>
+          <div class="grid-content gird-5">
+            <div class="gird-wrap" @click="linkModule('/projectadmin')" data-url="">
+              <div class="trans-middle f-mid">
                 <p class="p1"><i class="iconfont">&#xe63b;</i></p>
                 <p>项目管理</p>
               </div>
+            </div>
+          </div>
+          <div class="grid-content gird-6">
+            <div class="gird-wrap">
+              <div class="trans-middle f-mid">
+                <p class="p1"><i class="iconfont">&#xe603;</i></p>
+                <p>用户中心</p>
               </div>
-            </router-link>
-          </el-col>
-        </el-row>
+            </div>
+          </div>
+        </div>
+        <div class="grid-content gird-7">
+          <div class="gird-wrap">
+            <div class="trans-middle">
+              <p class="p1"><i class="iconfont">&#xe691;</i></p>
+              <p>道路照明系统</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-content gird-8">
+          <div class="gird-wrap">
+            <div class="trans-middle">
+              <p class="p1"><i class="iconfont">&#xe6f5;</i></p>
+              <p>能耗分析</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -89,160 +88,194 @@
 <script>
 export default {
   name: 'index',
-  data () {
-    return {
-      WindowHeight: 600,
-      blockHeight: 250,
-      blockHeight2: 115
-    }
-  },
-  computed: {},
-  created () {
-    this.WindowHeight = window.innerHeight
-    this.blockHeight = (this.WindowHeight - 113 - 70 - 20) / 2
-    this.blockHeight2 = (this.blockHeight - 20) / 2
-  },
   methods: {
+    linkModule: function (url) {
+      console.log(url)
+      if (url) this.$router.push(url)
+    }
   }
 }
 </script>
-<style rel="stylesheet/scss" lang="scss">
-/* reset element-ui css */
-.system-home{
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  .el-col {
-  }
-  .grid-content {
-    min-height: 36px;
-  }
-}
-</style>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-#app{
+<style lang="scss" scoped>
+.container{
   width: 100%;
   height: 100%;
-  min-width: 1200px;
-}
-.home-absolute{
-  width:1200px;
-  margin:0 auto;
+  min-width: 1000px;
+  margin: 0 auto;
+  background: url('../../assets/index/index_bg.jpg') fixed center top;
+  background-size:cover;
+  position: relative;
+  overflow: hidden;
 }
 
-.home-top{
-  padding:30px;
-  .meau-icon{
+.index-header{
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 20px 20px 0;
+  .menu-link{
     color: #ffffff;
-    i{
+    .iconfont{
       font-size:24px;
     }
   }
   .user-info{
+    cursor: pointer;
     img{
       width: 50px;
       height: 50px;
+      line-height: 50px;
       margin-right:10px;
+      vertical-align: middle;
     }
-    .text-info{
-      float: right;
+    span{
       font-size:14px;
       color: #fff;
-      line-height: 25px;
-      p{
-        margin: 0;
-      }
     }
   }
 }
-.system-home{
-  width: 100%;
-  min-height: 100%;
-  min-width: 1080px;
-  margin: 0 auto;
-  background: url('../../assets/index/bg.png') fixed center top;
-  background-size:cover;
-  position: relative;
+.index-module{
+  width: 120vh;
+  margin: 10vh auto;
 }
-.home-center{
-  padding:20px 130px 50px 130px;
-  .mode-block{
-    height: 250px;
-    .el-col{
-      height: 250px;
-    }
-    .grid-content{
-      position: relative;
-      text-align:center;
-      color:#fff;
-      background-size:cover;
-      font-size: 18px;
-      p{
-        margin: 10px 0;
-      }
-      .trans-middle{
-        position: absolute;
-        top:50%;
-        left:50%;
-        width:100%;
-        text-align:center;
-        transform:translate(-50%,-50%);
-        -ms-transform:translate(-50%,-50%);
-        p{
-          margin-top: 10px;
-          margin:0;
-        }
-      }
-    }
-    .model-a{
-      height: 100%;
-      background-image: url('../../assets/index/1.png');
-    }
-    .model-b{
-      height: 100%;
-      background-image: url('../../assets/index/2.png');
-    }
-    .model-c{
-      height: 100%;
-      background-image: url('../../assets/index/3.png');
-    }
-    .model-d{
-      height: 115px;
-      margin-bottom: 20px;
-      background-image: url('../../assets/index/4.png');
-    }
-    .model-d1{
-      height: 115px;
-      background-image: url('../../assets/index/5.png');
-    }
-    .model-e{
-      height: 100%;
-      background-image: url('../../assets/index/6.png');
-    }
-    .model-f{
-      height: 100%;
-      background-image: url('../../assets/index/7.png');
-    }
-    .model-g{
-      height: 100%;
-      background-image: url('../../assets/index/8.png');
-    }
-    .p1{
-      padding-bottom: 5px;
-    }
+.grid-content{
+  float: left;
+  padding: 5px;
+  position: relative;
+  text-align:center;
+  color:#fff;
+  font-size: 18px;
+  transition: all .4s ease;
+  box-sizing: border-box;
+  cursor: pointer;
+  .gird-wrap{
+    height: 100%;
+    width: 100%;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    box-sizing:border-box;
+    transition: opacity, .2s ease;
     .iconfont{
+      display: inline-block;
       font-size: 60px;
-      &.f-mid{
+    }
+    &:hover{
+      .trans-middle{
+        transform: translate(-50%,-50%) scale(1.08);
+      }
+    }
+  }
+  .trans-middle{
+    position: absolute;
+    top:50%;
+    left:50%;
+    width:100%;
+    text-align:center;
+    transform:translate(-50%,-50%);
+    transition: all .4s ease;
+    p{
+      padding-top: 10px;
+    }
+    &.f-mid{
+      .iconfont{
         font-size: 48px;
       }
+      p{
+        padding-top: 5px;
+      }
     }
   }
-  .grid-content{
-    cursor: pointer;
+  &.gird{
+    &-1{
+      height: 40vh;
+      width: 80vh;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_1.png');
+      }
+    }
+    &-2{
+      height: 40vh;
+      width: 40vh;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_2.png');
+      }
+    }
+    &-3{
+      height: 20vh;
+      width: 20vh;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_3.png');
+      }
+    }
+    &-4{
+      height: 20vh;
+      width: 20vh;
+      animation: flip 6s linear infinite;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_4.png');
+      }
+    }
+    &-5{
+      height: 20vh;
+      width: 20vh;
+      animation: flip 6s linear 4s infinite;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_5.png');
+      }
+    }
+    &-6{
+      height: 20vh;
+      width: 20vh;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_6.png');
+      }
+    }
+    &-7{
+      height: 40vh;
+      width: 40vh;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_7.png');
+      }
+    }
+    &-8{
+      height: 40vh;
+      width: 40vh;
+      .gird-wrap{
+        background-image: url('../../assets/index/index_m_8.png');
+      }
+    }
+    &-9{
+      padding: 0;
+      height: 40vh;
+      width: 40vh;
+    }
   }
+}
+
+@keyframes flip {
+    0% {
+        transform: rotateX(0deg);
+    }
+    15% {
+        transform: rotateX(360deg);
+    }
+    100% {
+        transform: rotateX(360deg);
+    }
+}
+@keyframes fade {
+    0% {
+        opacity: 0;
+    }
+    10% {
+        opacity: 1;
+    }
+    50% {
+        opacity: 1;
+    }
+    60% {
+        opacity: 0;
+    }
 }
 </style>
