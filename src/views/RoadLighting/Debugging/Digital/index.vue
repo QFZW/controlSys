368fdfa0575cb608bad167bc5e8db245e50de02e
muyichenran <961774108@qq.com>
2018-05-27 17:26:01 +0800

    no message

--- src/views/RoadLighting/Debugging/Digital/index.vue
+++ src/views/RoadLighting/Debugging/Digital/index.vue
@@ -13,14 +13,14 @@
     </div>
     <div class="system-center">
       <div class="operation-bar">
-        <el-button @click="addCabinet()" type="primary">调整满载功率</el-button>
-        <el-button>参数</el-button>
-        <el-button>号码</el-button>
-        <el-button>查时</el-button>
-        <el-button>同步</el-button>
-        <el-button>重启</el-button>
-        <el-button>复位</el-button>
-        <el-button>升级</el-button>
+        <el-button @click="adjustedPower()" type="primary">调整满载功率</el-button>
+        <el-button @click="parameterFun()">参数</el-button>
+        <el-button @click="telFun()">号码</el-button>
+        <el-button @click="checkedTime()">查时</el-button>
+        <el-button @click="synchronizationFun()">同步</el-button>
+        <el-button @click="restartFun()">重启</el-button>
+        <el-button @click="resetFun()">复位</el-button>
+        <el-button @click="upgradeFun()">升级</el-button>
       </div>
       <div class="data-list">
         <el-table
@@ -107,6 +107,173 @@
         </el-pagination>
       </div>
     </div>
+    <!-- 调整满载功率 -->
+    <el-dialog
+      title="调整满载功率"
+      :visible.sync="adjustedPowerShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="center">
+          <el-radio v-model="radio" label="1">使用灯具额定总功率作为满载功率（调整数量：1）</el-radio>
+          <el-radio v-model="radio" label="2">查询并使用电表功率作为满载功率（在线数量：1）</el-radio>
+        </div>
+        <div class="waring-text">
+          <i class="el-icon-warning"></i>此数据将用于后续的报表节能统计，请慎重修改。若使用电表功率请先在电表参数设置内选择用于“数据统计”的电表
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="adjustedPowerShow = false">取 消</el-button>
+        <el-button type="primary" @click="adjustedPowerShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 参数 -->
+    <el-dialog
+      title="参数"
+      :visible.sync="parameterShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要执行查询操作吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：执行查询后当前配置数据将会被查询结果代替</p>
+          </div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="parameterShow = false">取 消</el-button>
+        <el-button type="primary" @click="parameterShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 号码 -->
+    <el-dialog
+      title="号码"
+      :visible.sync="telShow"
+      center>
+      <div class="adjusted-tk-center">
+        <el-form :model="telForm" :rules="telrules" ref="telRuleForm" label-width="70px">
+          <el-form-item label="手机号码" prop="tel">
+            <el-input v-model="telForm.tel"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <div class="waring-text">
+              <i class="el-icon-warning"></i>
+              <div class="waring-center">
+                <p class="text1">请输入接收查询结果短信手机号码（共有在线集中器1台）</p>
+              </div>
+            </div>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="telShow = false">取 消</el-button>
+        <el-button type="primary" @click="telShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 查时 -->
+    <el-dialog
+      title="查时"
+      :visible.sync="checkedTimeShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要执行查询操作吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：执行查询后当前配置数据将会被查询结果代替</p>
+          </div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="checkedTimeShow = false">取 消</el-button>
+        <el-button type="primary" @click="checkedTimeShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 同步 -->
+    <el-dialog
+      title="同步"
+      :visible.sync="synchronizationShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text border-bottom">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要同步参数到集中器吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：下发完成后需要重启集中器才能使配置生效</p>
+          </div>
+        </div>
+        <div class="check-text paddingtop15">
+          <el-checkbox v-model="tbChecked">服务器地址和端口不可用时禁止同步</el-checkbox>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="synchronizationShow = false">取 消</el-button>
+        <el-button type="primary" @click="synchronizationShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 重启 -->
+    <el-dialog
+      title="重启"
+      :visible.sync="restartShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要执行查询操作吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：执行查询后当前配置数据将会被查询结果代替</p>
+          </div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="restartShow = false">取 消</el-button>
+        <el-button type="primary" @click="restartShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 复位 -->
+    <el-dialog
+      title="复位"
+      :visible.sync="resetShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要执行查询操作吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：执行查询后当前配置数据将会被查询结果代替</p>
+          </div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="resetShow = false">取 消</el-button>
+        <el-button type="primary" @click="resetShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 升级 -->
+    <el-dialog
+      title="升级"
+      :visible.sync="upgradeShow"
+      center>
+      <div class="adjusted-tk-center">
+        <el-form label-width="70px">
+          <el-form-item label="升级文件" prop="tel">
+            <el-button>上传<i class="el-icon-upload el-icon--right"></i></el-button>
+            <p class="upgrade-tishi">请选择并上传集中器升级文件（.tra.z）</p>
+          </el-form-item>
+          <el-form-item>
+            <div class="check-text">
+              <el-checkbox v-model="tbChecked">传输失败之后自动续传直至完成</el-checkbox>
+              <el-checkbox v-model="tbChecked">升级完成后自动重启集中器</el-checkbox>
+              <el-checkbox v-model="tbChecked">重启集中器后自动查询版本和参数</el-checkbox>
+            </div>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="upgradeShow = false">取 消</el-button>
+        <el-button type="primary" @click="upgradeShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -114,12 +281,40 @@
 export default {
   name: 'Digital',
   data () {
+    var validateTel = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('手机号码不得为空'))
+      }
+      var reg = /^1[3|4|5|7|8][0-9]{9}$/ // 验证规则
+      var flag = reg.test(value) // true
+      if (!flag) {
+        return callback(new Error('请输入正确的手机号码'))
+      }
+    }
     return {
+      radio: null,
       pageNumber: 1,
       pageSize: 100,
       multipleSelection: [],
       currentPage: 1,
-      ElectricMeterList: []
+      ElectricMeterList: [],
+      adjustedPowerShow: false,
+      parameterShow: false,
+      checkedTimeShow: false,
+      synchronizationShow: false,
+      tbChecked: true,
+      restartShow: false,
+      resetShow: false,
+      telShow: false,
+      telForm: {
+        tel: null
+      },
+      telrules: {
+        tel: [
+          { validator: validateTel, trigger: 'blur' }
+        ]
+      },
+      upgradeShow: false
     }
   },
   methods: {
@@ -138,6 +333,37 @@ export default {
     editRow () {
     },
     onSubmit () {
+    },
+    // 调整满载功率
+    adjustedPower () {
+      this.adjustedPowerShow = true
+    },
+    // 参数
+    parameterFun () {
+      this.parameterShow = true
+    },
+    // 号码
+    telFun () {
+      this.telShow = true
+    },
+    // 查时
+    checkedTime () {
+      this.checkedTimeShow = true
+    },
+    // 同步
+    synchronizationFun () {
+      this.synchronizationShow = true
+    },
+    // 重启
+    restartFun () {
+      this.restartShow = true
+    },
+    // 复位
+    resetFun () {
+      this.restartShow = true
+    },
+    upgradeFun () {
+      this.upgradeShow = true
     },
     // 弹窗关闭时将数据清空
     handleCloseDialog (done) {
@@ -164,6 +390,15 @@ export default {
   line-height: 70px;
   font-size: 16px
 }
+.el-radio__label{
+    font-size:16px;
+}
+.el-dialog__footer{
+  padding-top: 20px
+}
+.el-form-item__content{
+  line-height: normal
+}
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
@@ -173,5 +408,49 @@ export default {
         padding: 0 30px 0 55px;
     }
   }
+}
+.adjusted-tk-center{
+  padding:0 20px;
+  .el-radio+.el-radio{
+    margin-left: 0;
+    margin-bottom: 10px;
+  }
+  .center{
+    text-align: center;
+  }
+}
+.border-bottom{
+  padding-bottom:15px;
+  border-bottom: 1px solid #e5e5e5;
+}
+.waring-text{
+  padding-left: 20px;
+  position: relative;
+  font-size:16px;
+  i{
+    position: absolute;
+    left:0;
+    top:5px;
+    color:#66b1ff
+  }
+  .huise-text2{
+    font-size:14px;
+    color:#95989a;
+    margin-top: 10px;
+  }
+}
+.paddingtop15{
+  padding-top: 15px;
+}
+.check-text{
+  .el-checkbox+.el-checkbox{
+    margin-left: 0;
+    margin-top: 5px;
+  }
+}
+.upgrade-tishi{
+  margin-top: 5px;
+  font-size:12px;
+  color: #95989a;
 }
 </style>
--- src/views/RoadLighting/Debugging/ElectricMeter/index.vue
+++ src/views/RoadLighting/Debugging/ElectricMeter/index.vue
@@ -13,11 +13,11 @@
     </div>
     <div class="system-center">
       <div class="operation-bar">
-        <el-button @click="addCabinet()" type="primary">批量编辑</el-button>
-        <el-button>参数</el-button>
+        <el-button @click="editElectricity()" type="primary">批量编辑</el-button>
+        <el-button @click="parameterFun()">参数</el-button>
         <el-button>电表地址</el-button>
-        <el-button>同步</el-button>
-        <el-button>初始化电费</el-button>
+        <el-button @click="synchronizationFun()">同步</el-button>
+        <el-button @click="InitializationFun()">初始化电费</el-button>
       </div>
       <div class="data-list">
         <el-table
@@ -104,6 +104,146 @@
         </el-pagination>
       </div>
     </div>
+    <!-- 批量编辑 -->
+    <el-dialog title="批量编辑"
+      :visible.sync="editElectricityShow" :close-on-click-modal='false' :close-on-press-escape='false' center
+      :before-close="handleCloseDialog">
+        <el-form ref="form" label-width="110px">
+          <el-form-item label="电压倍率" required>
+            <el-input class="width350" v-model="newElectricity.projectName"></el-input>
+            <p class="input-tishi">连接到电表的电压互感器倍率</p>
+          </el-form-item>
+          <el-form-item label="电流倍率" required>
+            <el-input class="width350" v-model="newElectricity.latitude"></el-input>
+            <p class="input-tishi">连接到电表的电流互感器倍率</p>
+          </el-form-item>
+          <el-form-item label="电费单价" required>
+            <el-input class="width350" v-model="newElectricity.longitude"></el-input>
+            <p class="input-tishi">用于电费的急速按</p>
+          </el-form-item>
+          <el-form-item label="初始有功电能" required>
+            <el-input class="width350" v-model="newElectricity.latitude"></el-input>
+            <p class="input-tishi">(KWh)将在统计时把此数据增加到电表有功电能内</p>
+          </el-form-item>
+          <el-form-item label="初始无功电能" required>
+            <el-input class="width350" v-model="newElectricity.longitude"></el-input>
+            <p class="input-tishi">(KWh)将在统计时把此数据增加到电表无功电能内</p>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="newElectricity.state">自动查询(开灯或关灯后自动查询电表数据)</el-checkbox>
+            <el-checkbox v-model="newElectricity.state1">数据统计(此电表数据将用于能耗报表统计)</el-checkbox>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="editElectricityShow = false">取 消</el-button>
+          <el-button type="primary" @click="onSubmit">确 定</el-button>
+        </div>
+    </el-dialog>
+    <!-- 参数 -->
+    <el-dialog
+      title="参数"
+      :visible.sync="parameterShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要执行查询操作吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：执行查询后当前配置数据将会被查询结果代替</p>
+          </div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="parameterShow = false">取 消</el-button>
+        <el-button type="primary" @click="parameterShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 同步 -->
+    <el-dialog
+      title="同步"
+      :visible.sync="synchronizationShow"
+      center>
+      <div class="adjusted-tk-center">
+        <div class="waring-text border-bottom">
+          <i class="el-icon-warning"></i>
+          <div class="waring-center">
+            <p class="text1">确定要同步参数到集中器吗？（共有在线集中器1台）</p>
+            <p class="huise-text2">注意：下发完成后需要重启集中器才能使配置生效</p>
+          </div>
+        </div>
+        <div class="check-text paddingtop15">
+          <el-checkbox v-model="tbChecked">服务器地址和端口不可用时禁止同步</el-checkbox>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="synchronizationShow = false">取 消</el-button>
+        <el-button type="primary" @click="synchronizationShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 初始化电费 -->
+    <el-dialog
+      title="初始化电费"
+      :visible.sync="InitializationShow"
+      center>
+      <div class="initialization-tk">
+        <p class="p1">电能历史记录时间:2017:10:12-11111</p>
+        <div class="tk-top clearfix">
+          <a class="f-l"><i class="iconfont ">&#xe648;</i>增加时段</a>
+          <a class="f-l danger-a"><i class="iconfont">&#xe632;</i>删除时段</a>
+          <div class="rmb-select f-r">
+            货币单位
+            <el-select  placeholder="请选择">
+              <!-- <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option> -->
+            </el-select>
+          </div>
+        </div>
+        <div class="data-list">
+          <el-table
+            ref="multipleTable"
+            :data="ElectricMeterList"
+            style="width: 100%"
+            header-row-class-name="datalist-header"
+            @selection-change="handleSelectionChange">
+            <el-table-column
+              type="selection"
+              width="40">
+            </el-table-column>
+            <el-table-column
+              label="行号"
+              width="50">
+            </el-table-column>
+            <el-table-column
+              prop="provinceName"
+              label="开始时间"
+              width="110">
+            </el-table-column>
+            <el-table-column
+              prop="cityName"
+              label="结束时间"
+              width="110">
+            </el-table-column>
+            <el-table-column
+              label="单价"
+              width="70">
+            </el-table-column>
+            <el-table-column
+              prop=""
+              label="累计">
+            </el-table-column>
+          </el-table>
+        </div>
+        <p class="p2">合计电费:11100</p>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="InitializationShow = false">取 消</el-button>
+        <el-button type="primary" @click="InitializationShow = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -116,7 +256,13 @@ export default {
       pageSize: 100,
       multipleSelection: [],
       currentPage: 1,
-      ElectricMeterList: []
+      ElectricMeterList: [],
+      newElectricity: {},
+      editElectricityShow: false,
+      parameterShow: false,
+      synchronizationShow: false,
+      InitializationShow: false,
+      tbChecked: false
     }
   },
   methods: {
@@ -127,14 +273,25 @@ export default {
       this.pageNumber = val
       // 翻页请求
     },
-    addCabinet () {
-      this.cabinetDialog = true
+    editElectricity () {
+      this.editElectricityShow = true
     },
     deleteRow () {
     },
     editRow () {
     },
     onSubmit () {
+    },
+    // 参数
+    parameterFun () {
+      this.parameterShow = true
+    },
+    // 同步
+    synchronizationFun () {
+      this.synchronizationShow = true
+    },
+    InitializationFun () {
+      this.InitializationShow = true
     },
     // 弹窗关闭时将数据清空
     handleCloseDialog (done) {
@@ -161,6 +318,16 @@ export default {
   line-height: 70px;
   font-size: 16px
 }
+.el-dialog__footer{
+  padding-top: 20px
+}
+.el-form-item__content{
+  line-height: normal
+}
+.el-checkbox+.el-checkbox{
+  margin-left: 0;
+  margin-top: 10px;
+}
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
@@ -169,6 +336,84 @@ export default {
     .item-block{
         padding: 0 30px 0 55px;
     }
+  }
+}
+.adjusted-tk-center{
+  padding:0 20px;
+  .el-radio+.el-radio{
+    margin-left: 0;
+    margin-bottom: 10px;
+  }
+  .center{
+    text-align: center;
+  }
+}
+.border-bottom{
+  padding-bottom:15px;
+  border-bottom: 1px solid #e5e5e5;
+}
+.waring-text{
+  padding-left: 20px;
+  position: relative;
+  font-size:16px;
+  i{
+    position: absolute;
+    left:0;
+    top:5px;
+    color:#66b1ff
+  }
+  .huise-text2{
+    font-size:14px;
+    color:#95989a;
+    margin-top: 10px;
+  }
+}
+.paddingtop15{
+  padding-top: 15px;
+}
+.check-text{
+  .el-checkbox+.el-checkbox{
+    margin-left: 0;
+    margin-top: 5px;
+  }
+}
+.input-tishi{
+  margin-top: 5px;
+  font-size:12px;
+  color: #95989a;
+}
+.initialization-tk{
+  .el-table__body{
+    width: 100%;
+  }
+  .p1{
+    font-size:12px;
+    color:#67c23a
+  }
+  .p2{
+    margin-top: 10px;
+    font-size:12px;
+    color:#67c23a
+  }
+  .tk-top{
+    margin-top: 10px;
+    a{
+      display: inline-block;
+      padding-right: 12px;
+      font-size: 14px;
+      line-height: 40px;
+      color: #5789fa;
+      font-weight: normal;
+    }
+    .rmb-select{
+      width:240px;
+      .el-select{
+        width:160px
+      }
+    }
+  }
+  .data-list{
+    margin-top: 10px;
   }
 }
 </style>
