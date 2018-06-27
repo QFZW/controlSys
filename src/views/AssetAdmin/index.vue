03cc62be2ad47a12328d574d52a5f65dafa08297
yangming <961774108@qq.com>
2018-06-27 17:58:35 +0800

    no message

---
+++ src/views/AssetAdmin/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'AssetAdminIndex',
+  methods: {
+    linkModule: function (url) {
+      console.log(url)
+      if (url) this.$router.push(url)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
